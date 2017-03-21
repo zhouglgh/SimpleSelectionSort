6f3b563e63886d8d0ace9fac15b0c6953e4204ac
zhouglgh <zhou_303425@163.com>
2017-03-20 22:31:35 -0400

    添加双元选择排序

--- SimSelSort.cpp
+++ SimSelSort.cpp
@@ -1,6 +1,10 @@
 #include "SimSelSort.h"
 #define MAX_INT 0x7fffffff
+#define MIN_INT (-1)
+/*简单选择排序*/
 static void findTheSmallest(int *array,int size,int start);
+/*双元选择排序*/
+static void findTheSandB(int *array,int start,int end);
 
 int SSsort::sort(int *array,int size)
 {
@@ -8,8 +12,12 @@ int SSsort::sort(int *array,int size)
 }
 void SSsort::func(int *array, int size)
 {
-	findTheSmallest(array,size,0);
+	//findTheSmallest(array,size,0);
+	findTheSandB(array,0,size-1);
 }
+/*
+ * 找到最小的值放在最前
+ * */
 static void findTheSmallest(int *array,int size,int start)
 {
 	if(start < size-1)
@@ -30,4 +38,36 @@ static void findTheSmallest(int *array,int size,int start)
 	}
 	else
 		return;
+}
+/*
+ * 找到最小和最大分别放在最前和最后。
+ * */
+static void findTheSandB(int *array,int start,int end)
+{
+	if(start < end)
+	{
+		int i=start,smaller=MAX_INT,id_small=-1,largest=MIN_INT,id_large=-1;
+		while(i<=end)
+		{
+			if(array[i] < smaller)
+			{
+				smaller = array[i];
+				id_small = i;
+			}
+			if(array[i] > largest)
+			{
+				largest = array[i];
+				id_large = i;
+			}
+			i++;
+		}
+		if(array[start] != array[id_small])
+			array[start] ^= array[id_small] ^= array[start] ^= array[id_small];
+		if(array[end] != array[id_large])
+			array[end] ^= array[id_large] ^= array[end] ^= array[id_large];
+		findTheSandB(array,start+1,end-1);
+	}
+	else
+		return;
+	
 }
